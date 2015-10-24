5c99cfd4b9e11eb432f88d04bcd7fdd4e0b5a01a
cokasaki <cokasaki@hmc.edu>
2015-10-24 01:46:05 -0700

    printing

--- game/ais/test_2.py
+++ game/ais/test_2.py
@@ -10,15 +10,15 @@
 
 # do_turn takes the following inputs, packed into a list called 'state':
 
-#   tank_coords : coordinates of all tanks on the board in format: [name_1, x_pos_1, y_pos_1],[name_2, x_pos_2, y_pos_2], ... ]
+#   tank_coords : coordinates of all tanks on the board in format: {{name_1, x_pos_1, y_pos_1},{name_2, x_pos_2, y_pos_2}, ... }
 #   hp          : tank's current health
 #   x_pos       : tank's x-coordinate [0-63]
 #   y_pos       : tank's y-coordinate [0-63]
 #   been_hit    : boolean variable describing whether the tank has been hit
-#   aggressors  : all tanks that have shot you in the past turn, in format: [name1, name2, name3, ... ]
+#   aggressors  : all tanks that have shot you in the past turn, in format: {name1, name2, name3, ... }
 
 # do_turn returns the following outputs, stored in a list of form:
-#   [x_vel, y_vel, tx, ty]
+#   {x_vel, y_vel, tx, ty}
 
 #   x_vel, y_vel  : tank's velocity vector
 #   tx, ty        : tank's turret vector
@@ -26,11 +26,12 @@
 
 class TankAI:
 
-    def __init__(self,init_state):
+    def init(self,init_state):
         # do nothing
+        pass
 
-    def take_turn (self,state):
-        [tank_coords, hp, ammo, [x_pos, y_pos], aggressors] = state
+    def take_turn(self,state):
+        #[tank_coords, hp, ammo, [x_pos, y_pos], aggressors] = state
         x_vel = 0 # movement direction
         y_vel = 0
         tx    = 1 # turret direction
