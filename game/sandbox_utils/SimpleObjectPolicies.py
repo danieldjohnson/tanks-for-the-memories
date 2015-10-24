aa103f1a6ea7f9c69c582a5ac89a1f1c3bfa0c17
hexahedria <hexahedria@gmail.com>
2015-10-24 00:29:21 -0700

    Remove unneeded Record

--- game/sandbox_utils/SimpleObjectPolicies.py
+++ game/sandbox_utils/SimpleObjectPolicies.py
@@ -45,11 +45,6 @@ or in ZopeSecurityPolicy. :(
 
 _noroles = [] # this is imported in various places
 
-import Record
-
-# Allow access to unprotected attributes
-Record.Record.__allow_access_to_unprotected_subobjects__=1
-
 # ContainerAssertions are used by cAccessControl to check access to
 # attributes of container types, like dict, list, or string.
 # ContainerAssertions maps types to a either a dict, a function, or a
