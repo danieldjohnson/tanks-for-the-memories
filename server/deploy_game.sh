31701f0537c52e6015d12cec2ff05fa287c86ca3
hexahedria <hexahedria@gmail.com>
2015-11-01 22:17:19 -0800

    Update to remove JIT in pypy

--- server/deploy_game.sh
+++ server/deploy_game.sh
@@ -3,7 +3,7 @@ set -e
 cd ../game
 mv config.py ../game_config.py
 echo 'OUTPUT_LED = False\nOUTPUT_STDOUT = False\nUSE_SIMULATOR = True' >config.py
-cd ../server/public/pypyjs-release/
+cd ../server/public/pypyjs-release-nojit/
 git reset HEAD --hard
 python tools/module_bundler.py add ./lib/modules ../../../game
 cd ../../../
